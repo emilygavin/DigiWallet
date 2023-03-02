81733df2a12310a356b51ec552fb1e670ad35c08
emilygavin <emilygavin@hotmail.com>
2023-03-02 23:21:57 +0000

    Small schema change

--- src/main/java/com/example/DigiWallet/User.java
+++ src/main/java/com/example/DigiWallet/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class User {
     private String id;
+    private String name;
     private String email;
     private String password;
     private Cards cards;
